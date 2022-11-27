fe71bcb36551e529f658b2a15bb156d45a326a8f
Cezar Ionescu <->
2022-11-27 18:24:13 +0200

    Added POST method

--- dataHandling.js
+++ dataHandling.js
@@ -1,15 +1,41 @@
 // GET data using XMLHttpRequest
+function getData() {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://jsonplaceholder.typicode.com/users");
+    xhr.send();
+    xhr.responseType = "json";
+    xhr.onload = () => {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            const data = xhr.response;
+            console.log(data);
+        } else {
+            console.log(`Error: ${xhr.status}`);
+        }
+    };
+}
 
-const xhr = new XMLHttpRequest();
-xhr.open("GET", "https://jsonplaceholder.typicode.com/users");
-xhr.send();
-xhr.responseType = "json";
-xhr.onload = () => {
-    if (xhr.readyState == 4 && xhr.status == 200) {
-        const data = xhr.response;
-        console.log(data);
-    } else {
-        console.log('Error: ${xhr.status}');
-    }
-};
 
+// POST data using XMLHttpRequest
+function postData() {
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", "https://jsonplaceholder.typicode.com/posts");
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    const payload = JSON.stringify({
+        title: "Hello World",
+        body: "POST request",
+        userId: 700,
+    });
+    xhr.onload = () => {
+        if (xhr.readyState == 4 && xhr.status == 201) {
+            console.log(JSON.parse(xhr.responseText));
+        } else {
+            console.log(`Error: ${xhr.status}`);
+        }
+    };
+    xhr.send(payload);
+}
+
+
+// Run functions
+getData();
+postData();
