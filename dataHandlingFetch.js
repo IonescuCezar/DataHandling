ead32cd94a2a0e64cbae805beb499b5758aff53c
Cezar Ionescu <->
2022-12-11 15:07:28 +0200

    Added POST method using Fetch API

--- dataHandlingFetch.js
+++ dataHandlingFetch.js
@@ -1,9 +1,34 @@
-// GET data using Fetch
+// GET data using Fetch API
 function getData() {
     fetch("https://jsonplaceholder.typicode.com/posts")
         .then((response) => response.json())
         .then((data) => console.log("Successful GET: ", data));
 }
 
+
+// POST data using Fetch API
+function postData() {
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+        method: "POST",
+        body: JSON.stringify({
+            title: "Hello World",
+            body: "POST Request",
+            userId: 700,
+        }),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8",
+        },
+    })
+        .then((response) => response.json())
+        .then(data => console.log("Successful POST: ", data));
+}
+
+
 // Run function(s)
 getData();
+postData();
+
+
+
+
+
